b71ceedb91ad9974308194bf85a5d6b2f788c9db
Sakib <sakibulalam923@gmail.com>
2024-09-02 22:27:32 +0600

    Created Category list

--- src/components/ExploreMenu/ExploreMenu.jsx
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,5 +1,23 @@
-export default function ExploreMenu(){
+import "./ExploreMenu.css"
+import {menu_list} from '../../assets/assets';
+
+export default function ExploreMenu({category, setCategory}){
+
     return(
-        <div></div>
+        <div className="explore-menu" id="explore-menu">
+            <h1>Explore Menu</h1>
+            <p className="explore-menu-text">Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise. Our mission is to satisty your cravings and elevate your dining experience, one delicious meal at a time</p>
+            <div className="explore-menu-list">
+                { menu_list.map((item, index) =>{
+                    return (
+                        <div onClick={() => {setCategory(prev => prev===item.menu_name?"All":item.menu_name)}} key={index} className="explore-menu-list-item">
+                            <img className={category===item.menu_name?"active":""} src={item.menu_image} alt={item.menu_name} />
+                            <p> {item.menu_name} </p>
+                        </div>
+                    )
+                })}                    
+            </div>
+            <hr></hr>
+        </div>
     )
 }
